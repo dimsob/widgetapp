bd48281c39ad5a812a67e121214ae2c6c9a3fc70
dimsob <sobscorp2@mail.ru>
2016-08-08 17:33:16 +0700

    eqweqwe

---
+++ app/src/main/java/devalemedia/widgetapp/MyWidClass.java
@@ -0,0 +1,37 @@
+package devalemedia.widgetapp;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by admin on 08.08.2016.
+ */
+
+public class MyWidClass extends AppWidgetProvider {
+
+    final String LOG_TAG = "WidLgs";
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+        Log.d(LOG_TAG, "onEnabled");
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        Log.d(LOG_TAG, "onUpdate " + Arrays.toString(appWidgetIds));
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+        Log.d(LOG_TAG, "onDeleted " + Arrays.toString(appWidgetIds));
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+        Log.d(LOG_TAG, "onDisabled");
+    }
+}
